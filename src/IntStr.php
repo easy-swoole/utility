d709d663544cf4dd8ed49e9e2916658593c97231
仙士可 <31308307+tioncico@users.noreply.github.com>
2021-07-08 10:59:06 +0800

    高精度计算函数

--- src/IntStr.php
+++ src/IntStr.php
@@ -1,4 +1,5 @@
 <?php
+<?php
 
 
 namespace EasySwoole\Utility;
@@ -9,44 +10,44 @@ class IntStr
     public const intMax = 9223372036854775668;
 
     private const alphabet = [
-        'A','a','B','b','C','c','D','d','E', 'e',
-        'F','f','G','g','H','h','I','i','J','j',
-        'K','k','L','l','M','m','N','n','O','o',
-        'P','p','Q','q','R','r','S','s','T','t',
-        'U','u','V','v','W','w','X','x', 'Y','y',
-        'Z','z','0','1','2','3','4','5','6','7','8','9'
+        'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e',
+        'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j',
+        'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o',
+        'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't',
+        'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y',
+        'Z', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
     ];
 
-    public static function toAlpha($number):string
+    public static function toAlpha($number): string
     {
-        if($number < 0 && $number > self::intMax){
+        if ($number < 0 && $number > self::intMax) {
             throw new \InvalidArgumentException('number error');
         }
         $alpha = '';
-        if($number <= 61){
+        if ($number <= 61) {
             return self::alphabet[$number];
-        } elseif($number > 61){
+        } elseif ($number > 61) {
             $dividend = ($number + 1);
-            while ($dividend > 0){
+            while ($dividend > 0) {
                 $modulo = ($dividend - 1) % 62;
-                if($modulo < 0){
-                    $modulo = 62+$modulo;
+                if ($modulo < 0) {
+                    $modulo = 62 + $modulo;
                 }
                 $alpha = self::alphabet[$modulo] . $alpha;
-                $dividend = floor((($dividend - $modulo) / 62));
+                $dividend = bcdiv(($dividend - $modulo) , 62);
             }
         }
         return $alpha;
     }
 
-    public static function toNum($string):int
+    public static function toNum($string): int
     {
         $alpha_flip = array_flip(self::alphabet);
         $return_value = -1;
         $length = strlen($string);
         for ($i = 0; $i < $length; $i++) {
             $return_value +=
-                ($alpha_flip[$string[$i]] + 1) * pow(62, ($length - $i - 1));
+                ($alpha_flip[$string[$i]] + 1) * bcpow(62, ($length - $i - 1));
         }
         return $return_value;
     }
